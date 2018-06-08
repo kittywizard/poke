da57265623331b9d479dc5aa775212264343992f
marleigh <volcana@gmail.com>
2018-06-07 21:03:22 -0700

    It works. Now break it.

--- script.js
+++ script.js
@@ -4,12 +4,16 @@ var pokeResult; //junk variable probably
 //event listener for the button to grab search results
 var searchForPoke = document.getElementById('search').addEventListener('click', function() {
   pokeResult = document.getElementById('pokemonSearch').value;
+
+  console.log(pokeResult);
+  var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + pokeResult + "/";
+  console.log(pokeURL);
+  apiCall(pokeURL);
+
 });
 
-var pokeURL = "https://pokeapi.co/api/v2/pokemon/3";
-
 //create a function to call the API and get teh data
-function apiCall() {
+function apiCall(pokeURL) {
   const pokemonResult = {
     //the URL will need to be edited to account for search results later..
     url: pokeURL,
@@ -18,14 +22,32 @@ function apiCall() {
     success: function(data) { //put a real function call in here
       var test = data.name;
       console.log(test);
+
     }
   };
 
   //this jquery thing makes the call so you don't have to
   $.ajax(pokemonResult);
 }
-apiCall();
 
+function editHTML() {
+
+}
+
+function testFunction(data){
+  var test = data.name;
+  console.log(test);
+}
+//this is a new event listener using jquery
+/* function submit() {
+  $(.'form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('#pokemonSearch');
+    const query = queryTarget.val();
+
+  });
+}
+$(submit); */
 /*
 var url = "https://pokeapi.co/api/v2/pokemon/25";
 
