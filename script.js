4d1b9b960655332b83cc55b8e0f6ab82e1c0809d
marleigh <volcana@gmail.com>
2018-06-17 15:20:45 -0700

    Starting to style

--- script.js
+++ script.js
@@ -7,39 +7,41 @@ var searchForPoke = document.getElementById('search').addEventListener('click', 
   //console.log(pokeResult);
   var pokeURL = "https://pokeapi.co/api/v2/pokemon/" + pokeResult + "/";
   //console.log(pokeURL);
-  apiCall(pokeURL);
+  apiCall(pokeURL, editPage);
 
 });
 
 //create a function to call the API and get teh data
-function apiCall(pokeURL) {
+function apiCall(pokeURL, callback) {
   const pokemonResult = {
     //the URL will need to be edited to account for search results later..
     url: pokeURL,
     dataType: 'json',
     type: 'GET',
-    success: function(data) { //put a real function call in here
-      var test = data.name;
-      console.log(test);
-      $('.display').html(`<h1>${data.name}</h1>
-        <p>${data.types}</p>
-        `);
-
-      //lookup what a template literal is
-      //it allows you to use `` and put in expressions, it's not jquery despite the $$ bullshit
-
-
+    success: callback,
+    error: function(){
+      console.log("you done messed up.");
+      //display error message on screen
+    }
       /* callback. when you call the original apiCall function, you can put two arguments
       one being the URL variable and one being the callback function
       that function can take the data as a success: callback and then
       do shit with it so its not here cluttering things up
       */
-
-    }
+    
   };
 
   //this jquery thing makes the call so you don't have to
   $.ajax(pokemonResult);
+}
+
+//function to serve as the apiCall's callback
+function editPage(data) {
+      var test = data.name;
+      console.log(test);
+      $('.display').html(`<h1>${data.name}</h1>
+        <p>${data.id}</p>
+        `);
 }
 
 //this is a new event listener using jquery
