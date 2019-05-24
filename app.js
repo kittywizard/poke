fbb1dce254b1d1acb9cd352a96e58f28c9daff07
marleigh <volcana@gmail.com>
2019-05-24 15:57:09 -0700

    Well.. it works

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 document.getElementById('search').addEventListener('click', getPokemon);
 
 function getPokemon(e){
-    const data = document.getElementById('pokemonSearch').value;
-    //console.log(data);
+    const data = document.getElementById('pokemonSearch').value.toLowerCase();
+
+    console.log(data);
 
     //creating a variable to start the API request
     const xhr = new XMLHttpRequest();
@@ -11,15 +12,17 @@ function getPokemon(e){
     xhr.open('GET',`https://pokeapi.co/api/v2/pokemon/${data}`, true);
 
     xhr.onload = function(){
+        //check to make sure status is good before moving on
         if(this.status === 200) {
-            console.log('its a start');
+            //grab data from API and parse
             const result = JSON.parse(this.responseText);
-            if (result.type === 'success') {
-                console.log(result.type);
-            }
+
+            //use result to pull in the rest of the data you want
+            console.log("Name: " + result.name);
+            console.log("ID: " + result.id);
         }
         else {
-            console.log('oops');
+            console.log('Something went terribly wrong.');
         }
     }
 
