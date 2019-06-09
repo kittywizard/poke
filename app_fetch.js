118eac2213323b9a5f45338b02d6b46f39e80e4c
marleigh <volcana@gmail.com>
2019-06-09 16:27:28 -0700

    Updating to GET with fetch

--- app_fetch.js
+++ app_fetch.js
@@ -1,1 +1,19 @@
-//testing git
+
+document.getElementById('search').addEventListener('click', getPokemon);
+
+function getPokemon() {
+    const pokemon = document.getElementById('pokemonSearch').value.toLowerCase();
+
+        return new Promise((resolve, reject) => {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+            .then(res => res.json())
+            .then(data => resolve(displayPokemon(data)))
+            .catch(err => reject(err));
+        });
+
+    }
+
+function displayPokemon(data) {
+    console.log("working?");
+}
+
