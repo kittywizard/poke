0bf1b0a9a6a848504a8435700b55822ee8dad0de
marleigh <volcana@gmail.com>
2019-06-16 18:01:21 -0700

    attempting to get moves

--- app_fetch.js
+++ app_fetch.js
@@ -1,56 +1,61 @@
+document.getElementById("search").addEventListener("click", getPokemon);
 
-document.getElementById('search').addEventListener('click', getPokemon);
-
-function get(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => resolve(displayPokemon(data)))
-        .catch(err => reject(err));
-    });
+function get(url, fun) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => resolve(fun(data)))
+      .catch(err => reject(alert('Type in an actual Pokemon name. ')));
+  });
 }
 
 function getPokemon() {
-    const pokemon = document.getElementById('pokemonSearch').value.toLowerCase();
-        
-        //error check for no entry
-        if(pokemon === ''){
-            let display = `Please enter a valid Pokemon name.`;
-            document.getElementById('display').innerHTML = display;
-            return;
-        }
+  const pokemon = document.getElementById("pokemonSearch").value.toLowerCase();
 
-        get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    }
+  //error check for no entry
+  if (pokemon === "") {
+    let display = `Please enter a valid Pokemon name.`;
+    document.getElementById("display").innerHTML = display;
+    return;
+  }
+
+  get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, displayPokemon);
+}
 
 function displayPokemon(data) {
-    
-    const pokeName = convertName(data.name); 
-    
-    console.log(data.sprites.front_default);
+  const pokeName = convertName(data.name);
 
-    let display = `<span class="headline">Name:</span> ${pokeName} <br>
+  let display = `<span class="headline">Name:</span> ${pokeName} <br>
                     <span class="headline">ID:</span> ${data.id} <br>
-                    <span class="headline">Abilities:</span> <ul>`;
+                    <span class="headline">Moves: <ul>`;
 
-  data.abilities.forEach(element => {
-        let eleName = element.ability.name.charAt(0).toUpperCase() + element.ability.name.slice(1);
-        display += ` <li>${eleName}</li>`;
-     });
-    display += `</ul>`;
+    // an array inside an array
+    console.log(data.moves[0].move.name);
 
-    let image = `<img src="${data.sprites.front_default}">`;
+    
+//   for (let i = 0; i <= 4; i++) {
+//     display += `<li> ${data.moves[i].name} </li>`;
+//   }
 
-    document.getElementById('display').innerHTML = display;
-    document.getElementById('image').innerHTML = image;
+  //   display += `<span class="headline">Abilities:</span> <ul>`;
+  //   data.abilities.forEach(element => {
+  //     let eleName =
+  //       element.ability.name.charAt(0).toUpperCase() +
+  //       element.ability.name.slice(1);
+  //     display += ` <li>${eleName}</li>`;
+  //   });
+  //   display += `</ul>`;
 
+  let image = `<img src="${data.sprites.front_default}">`;
+
+  document.getElementById("display").innerHTML = display;
+  document.getElementById("image").innerHTML = image;
 }
 
 function convertName(name) {
-    //return the name, with the first character upper case
-    //then slice the rest of the string at the second letter and put it back together
-    return name.charAt(0).toUpperCase() + name.slice(1);
-   
+  //return the name, with the first character upper case
+  //then slice the rest of the string at the second letter and put it back together
+  return name.charAt(0).toUpperCase() + name.slice(1);
 }
 /* some garbage div creation
     let newDiv = document.createElement('div');
